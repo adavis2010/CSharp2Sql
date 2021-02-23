0866632431e676627904b674ac8940f9610680de
max-student <max-student@maxtrain.com>
2021-02-23 10:37:37 -0500

    initial commit on connection class

---
+++ CSharp2Sql/Connection.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp2Sql {
+    public class Connection {
+
+        public SqlConnection connection { get; set; }
+
+        public void Connect(string database) {
+
+            var connsStr = $"server=localhost\\sqlexpress;" +
+                            $"database={database};" +
+                            $"trusted_connection=true;";
+            //create sql connection instance and pass in connection string
+            connection = new SqlConnection(connsStr);
+            //open connection
+            connection.Open();
+            //check connection state property
+            if (connection.State != System.Data.ConnectionState.Open) {
+
+                throw new Exception("Connection did not open!");
+            }
+        }
+        public void Disconnect() {
+            connection.Close();
+        }
+    }
+}
--- TestCSharp2Sql/Program.cs
+++ TestCSharp2Sql/Program.cs
@@ -3,19 +3,22 @@ using CSharp2Sql;
 namespace TestCSharp2Sql {
     class Program {
         static void Main(string[] args) {
-            
-
+            //create instance of class
+            var conn = new Connection();
+            conn.Connect("EdDb");
+            //closes connection
+            conn.Disconnect();
 
                 //create instance of class
 
-                var sql = new EdDbLib();
-                sql.Connect("EdDb");
-                Console.WriteLine("Connected Successfully");
+            // var sql = new EdDbLib();
+            //sql.Connect("EdDb");
+            //Console.WriteLine("Connected Successfully");
 
-                //sql.ExecSelect();
-                sql.Class();
-                sql.Disconnect();
-            }
+            //sql.ExecSelect();
+            //sql.Class();
+            //sql.Disconnect();
+        }
 
             //create instance of class
 
