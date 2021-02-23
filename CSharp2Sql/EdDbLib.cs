c2c5ef21cc2c77213931f6ff5caf33038e892864
max-student <max-student@maxtrain.com>
2021-02-22 21:00:07 -0500

    Commit

--- CSharp2Sql/EdDbLib.cs
+++ CSharp2Sql/EdDbLib.cs
@@ -5,49 +5,92 @@ using System;
 namespace CSharp2Sql {
     public class EdDbLib {
 
-        public SqlConnection connection { get; set;}
+        public SqlConnection connection { get; set; }
 
-        // sql command statement
-        public void ExecSelect() {
-            var sql = "SELECT * From Student;";
+        public void ExecSelect(object id, object lastname, object major, object majorid, object majorname) {
+            var sql = "SELECT * from Major;";
+
             var cmd = new SqlCommand(sql, connection);
-            //use execute reader with SELECT statements
+
             var reader = cmd.ExecuteReader();
             while (reader.Read()) {
-                var id = Convert.ToInt32(reader["Id"]);
-                var lastname = reader["Lastname"].ToString();
+                int majorid = Convert.ToInt32(reader["majorid"]);
+                var majorname = reader["majorname"].ToString();
 
-                Console.WriteLine($"id={id},lastname={lastname}");
             }
-            reader.Close();
-            
+
+            Console.WriteLine($"majorid={majorid}, majorname={majorname}");
+
         }
+            reader.Close();
 
-
-        //method that will allow us to connect to sql database
         public void Connect(string database) {
             var connStr = $"server=localhost\\sqlexpress;" +
                             $"database={database};" +
                                 "trusted_connection=true;";
 
-            //create instance to sql server
             connection = new SqlConnection(connStr);
-
-            //open connection
             connection.Open();
             if (connection.State != System.Data.ConnectionState.Open) {
                 throw new Exception("Connection is not open!");
             }
 
-        
-        }
-
             public void Disconnect() {
-            connection.Close();
+                connection.Close();
             }
 
-    }
 
 
 
-}
+            /*//sql command statement
+             public void ExecSelect() {
+                 var sql = "SELECT * From Student;";
+                 var cmd = new SqlCommand(sql, connection);
+                 //use execute reader with SELECT statements
+                 var reader = cmd.ExecuteReader();
+                 while (reader.Read()) {
+                     var id = Convert.ToInt32(reader["Id"]);
+                     var lastname = reader["Lastname"].ToString();
+
+                     Console.WriteLine($"id={id},lastname={lastname}");
+                 }
+                 reader.Close();
+
+             }
+
+
+             //method that will allow us to connect to sql database
+             public void Connect(string database) {
+                 var connStr = $"server=localhost\\sqlexpress;" +
+                                 $"database={database};" +
+                                     "trusted_connection=true;";
+
+                 //create instance to sql server
+                 connection = new SqlConnection(connStr);
+
+                 //open connection
+                 connection.Open();
+                 if (connection.State != System.Data.ConnectionState.Open) {
+                     throw new Exception("Connection is not open!");
+                 }
+
+
+             }
+
+                 public void Disconnect() {
+                 connection.Close();
+                 }
+
+         }
+
+
+     */
+        }
+    } }
+
+            
+        
+
+
+
+
--- TestCSharp2Sql/Program.cs
+++ TestCSharp2Sql/Program.cs
@@ -3,16 +3,27 @@ using CSharp2Sql;
 namespace TestCSharp2Sql {
     class Program {
         static void Main(string[] args) {
+            static void Main(string[] args) {
 
+
+                //create instance of class
+
+                var sql = new EdDbLib();
+                sql.Connect("EdDb");
+                Console.WriteLine("Connected Successfully");
+
+                sql.ExecSelect();
+                sql.Disconnect();
+            }
 
             //create instance of class
 
-            var sql = new EdDbLib();
-            sql.Connect("EdDb");
-            Console.WriteLine("Connected Successfully");
+            //var sql = new EdDbLib();
+            //sql.Connect("EdDb");
+            //Console.WriteLine("Connected Successfully");
 
-            sql.ExecSelect();
-            sql.Disconnect();
+            //sql.ExecSelect();
+            //sql.Disconnect();
         }
     }
 }
